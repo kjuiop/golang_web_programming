05264d587e87a64ddc1af9c01f80562c5b809903
kjuiop <copying_y@naver.com>
2022-08-22 18:34:52 +0900

    Revert "test: test refactoring"
    This reverts commit 51367d2f1b2b0fd237c591ce5d9c5ba2fdb6d9e7.

--- practice/golang_learning_test.go
+++ practice/golang_learning_test.go
@@ -3,7 +3,6 @@ package practice
 import (
 	"context"
 	"github.com/stretchr/testify/assert"
-	"strings"
 	"testing"
 	"time"
 )
@@ -11,37 +10,40 @@ import (
 // golang 학습 테스트
 func TestGolang(t *testing.T) {
 	t.Run("string test", func(t *testing.T) {
-		str := "Ann,Jenny,Tom,Zico"
-		actual := strings.Split(str, ",")
-		expected := []string{"Ann", "Jenny", "Tom", "Zico"}
-		assert.Equal(t, expected, actual)
+		//str := "Ann,Jenny,Tom,Zico"
+		//actual := "" // TODO str을 , 단위로 잘라주세요.
+		//expected := []string{"Ann","Jenny","Tom","Zico"}
+		//TODO assert 문을 활용해 actual과 expected를 비교해주세요.
 	})
 
 	t.Run("goroutine에서 slice에 값 추가해보기", func(t *testing.T) {
 		var numbers []int
 		for i := 0; i < 100; i++ {
 			go func() {
-				numbers = append(numbers, i)
+				// TODO numbers에 i 값을 추가해보세요.
 			}()
 		}
 
 		var expected []int // actual : [0 1 2 ... 99]
-		expected = numbers
+		// TODO expected를 만들어주세요.
 		assert.ElementsMatch(t, expected, numbers)
 	})
 
 	t.Run("fan out, fan in", func(t *testing.T) {
+		/*
+			TODO 주어진 코드를 수정해서 테스트가 통과하도록 해주세요!
+
+			- inputCh에 1, 2, 3 값을 넣는다.
+			- inputCh로 부터 값을 받아와, value * 10 을 한 후 outputCh에 값을 넣어준다.
+			- outputCh에서 읽어온 값을 비교한다.
+		*/
 
 		inputCh := generate()
 		outputCh := make(chan int)
 		go func() {
-			defer close(outputCh)
 			for {
 				select {
-				case value, ok := <-inputCh:
-					if !ok {
-						return
-					}
+				case value := <-inputCh:
 					outputCh <- value * 10
 				}
 			}
@@ -58,12 +60,11 @@ func TestGolang(t *testing.T) {
 	t.Run("context timeout", func(t *testing.T) {
 		startTime := time.Now()
 		add := time.Second * 3
-		ctx := context.TODO()
-		timeoutCtx, _ := context.WithTimeout(ctx, add)
+		ctx := context.TODO() // TODO 3초후에 종료하는 timeout context로 만들어주세요.
 
 		var endTime time.Time
 		select {
-		case <-timeoutCtx.Done():
+		case <-ctx.Done():
 			endTime = time.Now()
 			break
 		}
@@ -75,11 +76,10 @@ func TestGolang(t *testing.T) {
 		startTime := time.Now()
 		add := time.Second * 3
 		ctx := context.TODO() // TODO 3초후에 종료하는 timeout context로 만들어주세요.
-		deadlineCtx, _ := context.WithDeadline(ctx, time.Now().Add(add))
 
 		var endTime time.Time
 		select {
-		case <-deadlineCtx.Done():
+		case <-ctx.Done():
 			endTime = time.Now()
 			break
 		}
@@ -88,11 +88,9 @@ func TestGolang(t *testing.T) {
 	})
 
 	t.Run("context value", func(t *testing.T) {
-
-		ctx := context.Background()
-		ctx = context.WithValue(ctx, "key", "value")
-		assert.Equal(t, "value", ctx.Value("key"))
-		assert.Nil(t, ctx.Value("key1"))
+		// context에 key, value를 추가해보세요.
+		// 추가된 key, value를 호출하여 assert로 값을 검증해보세요.
+		// 추가되지 않은 key에 대한 value를 assert로 검증해보세요.
 	})
 }
 
